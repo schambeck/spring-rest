d63dff3b812a82c01003438960c0436aa9c84b7a
Marcelo Schambeck <mschambeck@gmail.com>
2021-11-15 22:32:56 -0300

    NoArgsConstructor removed

--- src/main/java/com/schambeck/rest/domain/Invoice.java
+++ src/main/java/com/schambeck/rest/domain/Invoice.java
@@ -2,7 +2,6 @@ package com.schambeck.rest.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
